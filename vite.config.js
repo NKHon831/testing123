b497907e1bff65f01c71204f02ac75ff3f999bf2
NKHon831 <khaihon030831@gmail.com>
2024-09-24 16:08:42 +0800

    asa

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    // build: {
-    //     outDir: "dist",
-    // },
+    build: {
+        outDir: "dist",
+    },
 });
