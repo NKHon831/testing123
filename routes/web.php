e882a7c697c6f87221a1572a26b49c0ddfbc78fc
NKHon831 <khaihon030831@gmail.com>
2024-10-07 16:26:23 +0800

    testing
    testing on how git kraken works

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,13 @@ use App\Http\Controllers\UserController;
 
 Route::get('/',action: [UserController::class, 'index'])->name('landing-page');
 
-Route::post('login', action: [AuthenticateController::class,'login'])->name('login');
+Route::post('login', action: [AuthenticateController::class,'login'])->name(name: 'login');
 
 Route::get('register', [AuthenticateController::class,'register'])->name('register-page');
 
 Route::post('register',[AuthenticateController::class,'createNewUser'])->name('register');
+
+Route::get('/home',[UserController::class,'home'])->name('home');
+
 
 
